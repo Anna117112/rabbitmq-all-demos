8c98a914fba62f0a78f2c1c7906b4483f29b1fe8
Anna <a.dyakonu85@mail.ru>
2023-01-12 22:20:50 +0300

    DZ_9

--- rabbitmq-console/src/main/java/com/flamexander/rabbitmq/console/consumer/FonoutConsumer.java
+++ rabbitmq-console/src/main/java/com/flamexander/rabbitmq/console/consumer/FonoutConsumer.java
@@ -4,8 +4,9 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-
-public class SimpleReceiverApp {
+// получает сообщения от producer
+public class FonoutConsumer {
+    // название очереди с которой работает
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
@@ -15,13 +16,14 @@ public class SimpleReceiverApp {
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // ждем сообщений
         System.out.println(" [*] Waiting for messages");
-
+// выполняем действие при получении сообщения
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
         };
-
+// ждем сообщений
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
         });
     }
--- rabbitmq-console/src/main/java/com/flamexander/rabbitmq/console/producer/Fonout.java
+++ rabbitmq-console/src/main/java/com/flamexander/rabbitmq/console/producer/Fonout.java
@@ -4,18 +4,24 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-
-public class SimpleSenderApp {
+//Lesson9 Spring_2
+public class Fonout {
+    // создаем очередь
     private final static String QUEUE_NAME = "hello";
+    // обменник
     private final static String EXCHANGER_NAME = "hello_exchanger";
 
     public static void main(String[] args) throws Exception {
+        // фабрика соединений
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
+            // создаем на сервере MQ
             channel.exchangeDeclare(EXCHANGER_NAME, BuiltinExchangeType.DIRECT);
+            // создаем очередь
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            // привязываем  EXCHANGER к очереди с ключом java
             channel.queueBind(QUEUE_NAME, EXCHANGER_NAME, "java");
 
             String message = "Hello World!";
